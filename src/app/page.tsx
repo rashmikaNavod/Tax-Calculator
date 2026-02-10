18c228b426b88776038279a4d90e3226c3b5f223
rashmikaNavod <drashmikanavod@gmail.com>
2026-02-10 07:01:57 +0530

    add next js project

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div className="h-screen bg-gray-100 flex items-center justify-center"><h1 className="text-4xl font-bold">US TAX CALCULATOR</h1></div>
+  )
+}
+
+export default page
